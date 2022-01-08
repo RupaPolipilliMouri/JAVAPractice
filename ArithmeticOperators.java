c8e8e3c31b0f8bb306e0329afaef29456dd8c983
RupaPolipilliMouri <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-01-08 20:52:39 +0530

    operatorsdemo

---
+++ ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package operatorsdemo;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float fvalue = 23.5f;
+		float fvalue1 = 45.345f;
+		System.out.println("Addition =" + (fvalue1 + fvalue));
+
+		System.out.println("division =" + (fvalue / fvalue1));
+		
+
+		System.out.println("Substraction =" + (fvalue1 - fvalue));
+		
+
+		System.out.println("Multiplication =" + (fvalue1 * fvalue));
+		
+	
+		System.out.println("Modulus =" + (fvalue1 % fvalue));
+	}
+
+}
